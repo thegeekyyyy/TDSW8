a10759258f5906ec3839038a4e10aafebf59b1ca
Aayushi Yadav <96073132+thegeekyyyy@users.noreply.github.com>
2023-04-19 22:54:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ a = st.number_input("Enter the first number")
 b = st.number_input("Enter the second number")
 c = st.number_input("Enter the third number")
 if st.button("Find Largest"):
-    result = largest_number(a,b.c)
+    result = largest_number(a,b,c)
     st.success(f"The largest number is {result}")
